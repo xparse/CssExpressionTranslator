91718f5ef243b899ed2a8c741dcfd8388f4a2f61
Ivan Scherbak <dev@funivan.com>
2017-08-22 10:48:44 +0300

    fixed #6 Fetch multiple attributes

--- src/CssExpressionTranslator.php
+++ src/CssExpressionTranslator.php
@@ -12,16 +12,20 @@
   class CssExpressionTranslator extends CssSelectorConverter implements ExpressionTranslatorInterface {
 
     /**
-     * @param string $cssExpr
+     * @param string $cssExpression
      * @return string
      */
-    public function convertToXpath($cssExpr) {
-      preg_match('!(.+) (@.+|.+\(\))$!', $cssExpr, $data);
-      if (empty($data[2])) {
-        return parent::toXPath($cssExpr);
+    public function convertToXpath($cssExpression) {
+      $xpathExpression = [];
+      foreach (explode(', ', $cssExpression) as $expression) {
+        preg_match('!(.+) (@.+|.+\(\))$!', $expression, $matchExpression);
+        if (!array_key_exists(2, $matchExpression)) {
+          $xpathExpression[] = parent::toXPath($expression);
+        } else {
+          $xpathExpression[] = parent::toXPath($matchExpression[1]) . '/' . $matchExpression[2];
+        }
       }
-
-      return parent::toXPath($data[1]) . '/' . $data[2];
+      return implode(' | ', $xpathExpression);
     }
 
 
